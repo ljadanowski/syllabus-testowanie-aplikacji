839ea2496c98b82d6ea610a627faa7653f019755
Wlodek Bzyl <matwb@ug.edu.pl>
2014-06-03 12:15:20 +0200

    cont. poprawki w {02,03}-wypozyczalnia.rb

--- wyklady/5/02-wypozyczalnia.rb
+++ wyklady/5/02-wypozyczalnia.rb
@@ -49,30 +49,25 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    total_amount, frequent_renter_points = 0.0, 0
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      # this_amount = element.charge
+      this_amount = element.charge
 
       frequent_renter_points += 1
       if element.movie.price_code = Movie::NEW_RELEASE && element.days_rented > 1
         frequent_renter_points += 1
       end
 
-      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
+      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      total_amount += this_amount
     end
 
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
-
-  def amount_for(rental)
-    rental.charge
-  end
-
 end
 
 # Przykład użycia
